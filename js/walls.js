40a8a8d2223b2573e4e5894388f4d1daab0a64f0
danielb07 <danielbarraud07@gmail.com>
2020-06-22 15:52:17 +0530

    you can now reset the game

--- js/walls.js
+++ js/walls.js
@@ -2,17 +2,16 @@ class Walls {
     constructor(){
         this.walls = [];
         this.wImg = []
-        this.border = [];
         // border walls
 
         // right wall 
-        this.border.push(new Wall(1292, 380,12,452))
+        this.walls.push(new Wall(1292, 380,12,452))
         // left wall
-        this.border.push(new Wall(240, 380,12,452))
+        this.walls.push(new Wall(240, 380,12,452))
         // bottom wall
-        this.border.push(new Wall(768, 600,1064,12))
+        this.walls.push(new Wall(768, 600,1064,12))
         // top wall
-        this.border.push(new Wall(768, 156,1064,12))
+        this.walls.push(new Wall(768, 156,1064,12))
         
         // // in-game walls
             // this.walls.push(new Wall(64, 502,320, 20))  
@@ -32,7 +31,7 @@ class Walls {
 
 
         this.walls.push(new Wall(1184, 264,200,32))
-        this.wImg.push(new WImg(1084, 248,HwallImg))
+        this.wImg.push(new WImg(1084, 248,HwallImg)) 
 
         this.walls.push(new Wall(940, 252,32,200))
         this.wImg.push(new WImg(924, 152,VwallImg))
@@ -48,14 +47,10 @@ class Walls {
             var wall = this.walls[i];
             wall.display();
         }
-        for(var k=0; k<this.wImg.length;k++){
-            var Img = this.wImg[k]
-            Img.display();
-        }
 
-        for(var j=0; j<this.border.length;j++){
-            var border = this.border[j];
-            border.display();
+        for(var i=0; i<this.wImg.length;i++){
+            var Img = this.wImg[i]
+            Img.display();
         }
 
         
